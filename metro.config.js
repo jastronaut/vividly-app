3dea4102b1e1093ac7856421460d550d8bc845ec
Jas Delgado <jas@jasdelgado.com>
2020-07-28 13:15:42 -0700

    config and package changes

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,8 @@
  * @format
  */
 
+ /*
+ old config
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
@@ -15,3 +17,21 @@ module.exports = {
     }),
   },
 };
+*/
+
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
+  };
+})();
