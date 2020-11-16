00e5c53415cd4d30e3bccfd4e2f9a6ad8c1b76e2
jastronaut <jas@jasdelgado.com>
2020-11-15 22:41:18 -0500

    draft postpage, comments

--- src/UserProfile/PostPage/PostPage.tsx
+++ src/UserProfile/PostPage/PostPage.tsx
@@ -1,35 +1,20 @@
 import React, { useState } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-import { User, PostPageProps, Post } from '../../interfaces';
+import CommentsContainer from './CommentsContainer';
+import PostContainer from './PostContainer';
+import { PostPageProps } from '../../Routes/interfaces';
+import { FriendUser, Post } from '../../types';
 import { ScreenContainer } from '../../styles';
-import {
-	PostContainer,
-	OPHeader,
-	OPNamesContainer,
-	OPProfilePicture,
-} from './styles';
 
 const PostPage = ({ navigation, route }: PostPageProps) => {
-	const [user, setUser] = useState<User>(route.params.user);
+	const [user, setUser] = useState<FriendUser>(route.params.user);
 	const [post, setPost] = useState<Post>(route.params.post);
-
-	const [comments, setComments] = useState();
 
 	return (
 		<ScreenContainer>
-			<PostContainer>
-				<OPHeader>
-						<OPProfilePicture
-							source={require('../../Home/pup.jpeg')}
-						/>
-						<OPNamesContainer>
-							<Text>{user.name}</Text>
-							<Text>@{user.username}</Text>
-						</OPNamesContainer>
-				</OPHeader>
-				<Text>Test</Text>
-			</PostContainer>
+			<PostContainer user={user} post={post} />
+			<CommentsContainer comments={post.comments} />
 		</ScreenContainer>
 	);
 };
